979374242981075f0b118c9931a3cb60bcb88f9b
unknown <manuela.rink@DESKTOP-V44FL4A.office.digenio.de>
2023-06-07 09:15:15 +0200

    pytest functions

--- database/db_store.py
+++ database/db_store.py
@@ -1,19 +1,29 @@
+''' Reads all the .csv-Files in data-dir and stores their contents to postgres-database.'''
 import sqlalchemy
 from sqlalchemy import create_engine
 from pathlib import Path
 import os
 import pandas as pd
 
-''' Reads all the .csv-Files in data-dir and stores their contents to postgres-database.'''
-engine = create_engine("postgresql+psycopg2://news:news@localhost:5432/news")
-with engine.connect() as conn:
 
-    # read all csv files and create one concatenated dataframe with a new column Id
-    # as primary key;
-    # then store this dataframe to db
+def connect(**kwargs) -> sqlalchemy.engine.mock.MockConnection:
+    '''
+    Establishes the db connection.
+    :param kwargs:
+    :return:
+    '''
+    engine = create_engine("postgresql+psycopg2://news:news@localhost:5432/news")
+    return engine.connect()
+
+
+def get_dataframe_from_csv(dir_path: Path) -> pd.DataFrame:
+    '''
+    Reads all csv files and creates one concatenated dataframe with a new column Id
+    as primary key.
+    '''
+
     df_concatenated = pd.DataFrame()
-    path = Path(f'{os.path.dirname(os.path.dirname(__file__))}/data/')
-    for csv_file in list(path.rglob('*.csv')):
+    for csv_file in list(dir_path.rglob('*.csv')):
 
         if csv_file.stat().st_size == 0:
             print(f'{csv_file} empty - skipping')
@@ -35,8 +45,17 @@ with engine.connect() as conn:
     # add a new column for the serial number
     #df_concatenated['Id'] = range(1, len(df_concatenated) + 1)
     print(df_concatenated)
+    return df_concatenated
 
-    # define table schema fro dataframe
+
+def store_to_db(conn: sqlalchemy.engine.mock.MockConnection, dataframe: pd.DataFrame) -> None:
+    '''
+    Stores data from dataframe to db by replacing the target table 'headlines'.
+    :param conn:
+    :param dataframe:
+    :return:
+    '''
+    # define table schema for dataframe
     table_schema = {'Date': sqlalchemy.DateTime,
                     'Title': sqlalchemy.String,
                     'Description': sqlalchemy.String,
@@ -49,9 +68,14 @@ with engine.connect() as conn:
                     #'Id': sqlalchemy.BigInteger}
 
     # write data to db
-    df_concatenated.to_sql('headlines', con=engine, if_exists='replace', dtype=table_schema, index=True)
+    dataframe.to_sql('headlines', con=conn, if_exists='replace', dtype=table_schema, index=True)
 
     # set primary key
-    engine.execute('ALTER TABLE headlines ADD PRIMARY KEY (index);')
+    conn.execute('ALTER TABLE headlines ADD PRIMARY KEY (index);')
 
 
+if __name__ == '__main__':
+    conn = connect()
+    path = Path(f'{os.path.dirname(os.path.dirname(__file__))}/data/')
+    df = get_dataframe_from_csv(path)
+    store_to_db(conn, df)
